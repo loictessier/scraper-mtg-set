0ae270bac07fb7fa5b4118093d7634454fd24e43
Loïc Tessier <loictessier.inge@gmail.com>
2024-08-30 23:21:26 +0200

    First script hard coded to download cards scan for specific set Mystery Booster 2 with specific naming

---
+++ scrap-set-cards-visuals.py
@@ -0,0 +1,18 @@
+import urllib.request
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()
+
+for i in range(1, 1742):
+    browser.get('https://www.magic-ville.com/fr/carte.php?ref=ms2' + str(i).zfill(3))
+
+    # get the image source
+    img = browser.find_element(By.XPATH, "//div[@id='CardScan']//img")
+    src = img.get_attribute('src')
+
+    # download the image
+    urllib.request.urlretrieve(src, "./output/MB2" + str(i).zfill(4))
+
+browser.close()
