464403588857fc4b9cec00b542abfcc7dcb06b59
Loïc Tessier <loictessier.inge@gmail.com>
2024-09-23 23:09:46 +0200

    Added english fields to the file builder for set tabler

--- scrap-set-cards-visuals.py
+++ scrap-set-cards-visuals.py
@@ -5,14 +5,23 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 
-for i in range(1, 1742):
-    browser.get('https://www.magic-ville.com/fr/carte.php?ref=ms2' + str(i).zfill(3))
+URL_ID = "a43"
+SET_ID = "DSKAS_"
+
+# DSK 1 à 417 puis 901 à 918
+# DSC 1 à 373 puis 901 à 923
+# DSKAS 1 à 108
+# a43
+card_counter = 1
+for i in range(1, 109):
+    browser.get('https://www.magic-ville.com/fr/carte.php?ref=' + URL_ID + str(i).zfill(3))
 
     # get the image source
     img = browser.find_element(By.XPATH, "//div[@id='CardScan']//img")
     src = img.get_attribute('src')
 
     # download the image
-    urllib.request.urlretrieve(src, "./output/MB2" + str(i).zfill(4) + ".jpg")
+    urllib.request.urlretrieve(src, "./output/" + SET_ID.upper() + str(card_counter).zfill(4) + ".jpg")
+    card_counter += 1
 
 browser.close()
--- scrap-set-tabler-file.py
+++ scrap-set-tabler-file.py
@@ -6,16 +6,49 @@ import requests
 
 
 def set_headers(sheet):
-    headers = ["Edition","Ref", "","Titre US","Titre FR","Cout","Cout converti","Couleur","Force","Endurance","Type US","Type FR","Sous type US","Sous type FR","Capacités ","Artiste","Regles US","Regles FR","Rarete"]
+    headers = ["Edition", "Ref", "", "Titre US", "Titre FR", "Cout", "Cout converti", 
+               "Couleur", "Force", "Endurance", "Type US", "Type FR", "Sous type US", 
+               "Sous type FR", "Capacités ", "Artiste", "Regles US", "Regles FR", "Rarete"]
     for i, c in enumerate(ascii_uppercase):
         if i == len(headers):
             break
         sheet[c + '1'] = headers[i]
 
-def build_file(filename):
+def build_file(filename, set_list):
     workbook = Workbook()
     sheet = workbook.active
     set_headers(sheet)
+    for i in range(1, len(set_list) + 1):
+        card = set_list[i-1]
+        sheet["A" + str(i + 1)] = card['set'].upper()
+        sheet["B" + str(i + 1)] = card['set'].upper() + str(card['collector_number'])
+        sheet["C" + str(i + 1)] = str(card['collector_number'])
+        sheet["D" + str(i + 1)] = card['name']
+        sheet["E" + str(i + 1)] = ""
+        sheet["F" + str(i + 1)] = card['mana_cost']
+        sheet["G" + str(i + 1)] = str(int(card['cmc']))
+        if len(card['colors']) > 0:
+            sheet["H" + str(i + 1)] = card['colors'][0] if len(card['colors']) <= 1 else 'Z'
+        elif "Artifact" in card['type_line'].split(' — ')[0]:
+            sheet["H" + str(i + 1)] = "A"
+        elif "Land" in card['type_line'].split(' — ')[0]:
+            sheet["H" + str(i + 1)] = "L"
+        else:
+            sheet["H" + str(i + 1)] = "C"
+        if 'power' in card:
+            sheet["I" + str(i + 1)] = card['power']
+        if 'toughness' in card:
+            sheet["J" + str(i + 1)] = card['toughness']
+        sheet["K" + str(i + 1)] = card['type_line'].split(' — ')[0]
+        sheet["L" + str(i + 1)] = ""
+        if len(card['type_line'].split(' — ')) > 1:
+            sheet["M" + str(i + 1)] = card['type_line'].split(' — ')[1]
+        sheet["N" + str(i + 1)] = ""
+        sheet["O" + str(i + 1)] = ""
+        sheet["P" + str(i + 1)] = card['artist']
+        sheet["Q" + str(i + 1)] = card['oracle_text']
+        sheet["R" + str(i + 1)] = ""
+        sheet["S" + str(i + 1)] = card['rarity']
     workbook.save(filename="./output/"+filename)
 
 def get_set(set_code):
@@ -39,14 +72,10 @@ def get_set(set_code):
                 next_page = None
         else:
             r = None
-
-    print(len(set_list))
     return set_list
 
-if __name__ == "__main__":
-    filename = "test.xlsx"
-    # build_file(filename)
-    set_list = get_set('blb')
+def test_api(set='blb'):
+    set_list = get_set(set)
     for card in set_list:
         # card fields : (upper)set (blb), ((upper)set+)collector_number (122), name (Agate Assault), (fr) printed_name , mana_cost ({2}{R}), cmc (3.0), colors (R), power (None), toughness (None), type_line(split[" — "][0]) (Sorcery), (fr) printed_type_line(split[" — "][0]) (),  type_line(split[" — "][1]) (Sorcery), (fr) printed_type_line(split[" — "][1]) (), (fr) printed_text, artist (Slawomir Maniak), oracle_text, (fr) printed_text, rarity(matching table "Commune", "Unco", "Rare", "Mythique")
         print("-----------------------")
@@ -70,3 +99,9 @@ if __name__ == "__main__":
         print(f"Artiste : { card['artist'] }")
         print(f"Regles US : { card['oracle_text'] }")
         print(f"Rarete : { card['rarity'] }")
+
+if __name__ == "__main__":
+    filename = "test.xlsx"
+    # test_api()
+    build_file(filename, get_set('blb'))
+    
